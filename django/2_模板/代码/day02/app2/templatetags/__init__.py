b3087b24ba503d916a6ba370d0e40c6d266cb973
csl <landmark_csl@126.com>
2020-01-14 17:02:17 +0800

    code

---
+++ django/2_模板/代码/day02/app2/templatetags/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @File    : __init__.py.py
+# 描述     ：
+# @Time    : 2020/1/14 15:01
+# @Author  : 
+# @QQ      :  
+
--- django/2_模板/代码/day02/day02/settings.py
+++ django/2_模板/代码/day02/day02/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'nu)ni2k0+-zd+)dqp5f*u(2+!47fp2ij9be%(xncy4gjd9_f-4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -124,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# 静态资源路径
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
