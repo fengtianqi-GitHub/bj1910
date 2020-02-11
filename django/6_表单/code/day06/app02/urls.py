7c926a698fc2e204abe66b561fee0929165d8468
csl <landkmark@126.com>
2020-02-11 17:54:51 +0800

    作业

--- django/6_表单/code/day06/app02/urls.py
+++ django/6_表单/code/day06/app02/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
+from App01 import views
+app_name = 'app02'
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('app01/',include("App01.urls")),
-    path('app02/',include("app02.urls")),
+    path('register/',views.register,name='register'),
 ]
--- django/6_表单/code/day06/day06/settings.py
+++ django/6_表单/code/day06/day06/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'App01.apps.App01Config',
+    'app02',
 ]
 
 MIDDLEWARE = [
